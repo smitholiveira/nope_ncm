5b68476344b5ed54df9dae56bf357f6c19534770
Smith Oliveira <olindex@gmail.com>
2022-12-15 22:05:01 +0000

    Adding pass.yml file and creating function func_yml based on yaml module.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import yaml
+
+
+def func_yml(file_path_yaml, group):
+    """ Read credentials from a yaml file. """
+    with open(file_path_yaml) as f:
+        file_yaml = f.read()
+    yaml_dict = yaml.load(file_yaml, Loader=yaml.FullLoader)
+    return yaml_dict[group]
 
 
 if __name__ == '__main__':
-    print_hi('Netmiko')
+    print(func_yml('pass.yml', 'devnet_iosxe'))
