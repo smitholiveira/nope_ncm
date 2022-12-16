a4b42c363917d8d4325b071c0a2fcf37f7cb8a33
Smith Oliveira <olindex@gmail.com>
2022-12-16 17:05:20 +0000

    Add class Login based on Netmiko module.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import yaml
+from netmiko import ConnectHandler, NetMikoAuthenticationException, NetMikoTimeoutException
+
+exceptions = (NetMikoAuthenticationException, NetMikoTimeoutException, Exception)
 
 
 def func_yml(file_path_yaml, group):
@@ -7,6 +10,33 @@ def func_yml(file_path_yaml, group):
         file_yaml = f.read()
     yaml_dict = yaml.load(file_yaml, Loader=yaml.FullLoader)
     return yaml_dict[group]
+
+
+class Login:
+    def __init__(self, var_credentials: dict, var_hosts: list, var_device_type):
+        # self.var_credentials = var_credentials
+        # self.var_hosts = var_hosts
+        # self.var_device_type = var_device_type
+
+        self.output = []
+
+        for hosts in var_hosts:
+            device = {
+                'device_type': var_device_type,
+                'host': hosts,
+                # 'global_delay_factor': 2,
+                # "read_timeout_override": 90,
+                'session_log': 'output_high-level.log'
+            }
+
+            device.update(var_credentials)
+
+            with ConnectHandler(**device) as net_connect:
+                net_connect.enable()
+                self.output.append(net_connect)
+
+    def login(self):
+        return self.output
 
 
 if __name__ == '__main__':
--- test_main.py
+++ test_main.py
@@ -1,12 +1,26 @@
 from unittest import TestCase
-from main import func_yml
+from main import func_yml, Login
+
+cred_iosxe = func_yml('pass.yml', 'devnet_iosxe')
+host_iosxe = ['sandbox-iosxe-recomm-1.cisco.com', 'sandbox-iosxe-latest-1.cisco.com']
+
+cred_nxos = func_yml('pass.yml', 'devnet_nxos')
+host_nxos = ['sandbox-nxos-1.cisco.com']
 
 
 class Test(TestCase):
-    def test_func_ini(self):
-        devnet_iosxe = func_yml('pass.yml', 'devnet_iosxe')
-        self.assertTrue(devnet_iosxe) # if it returns a True value
-        self.assertIsInstance(devnet_iosxe, dict)  # if it is a dictionary
+    def test_func_yml(self):
+        self.assertTrue(cred_iosxe)  # if it returns a True value
+        self.assertIsInstance(cred_iosxe, dict)  # if it is a dictionary
         # if it contains the following keys: username, password, secret
-        [self.assertIn(k, devnet_iosxe) for k in ['username', 'password', 'secret']]
+        [self.assertIn(k, cred_iosxe) for k in ['username', 'password', 'secret']]
         # self.fail()
+
+
+    def test_login(self):
+        device = Login(cred_nxos, host_nxos, 'cisco_nxos')
+        if device:
+            self.assertTrue(device.login())
+        else:
+            self.fail()
+
